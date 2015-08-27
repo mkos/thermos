a3868735a65a5c5f88760c981f842bbb860c5ac3
kosa <kosinski.michal@gmail.com>
2015-08-27 18:39:41 +0200

    added sending form data and session through flashes

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template, url_for
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "X\xc9i\xef\xf3'\x8a\xc0\x9d)~\xbbf\x89\xdd,C\x90[\xfe\xa41\xe3\xfd"
+bookmarks = []
+
+def store_bookmark(url):
+    bookmarks.append(dict(url = url,
+                          user = "mkos",
+                          date = datetime.utcnow()
+    ))
 
 @app.route('/')
 @app.route('/index')
@@ -9,8 +18,14 @@ def index():
                            title="Title passed from view to template",
                            text="Text passed from view to template")
 
-@app.route('/add')
+@app.route('/add', methods = ['GET', 'POST'])
 def add():
+    if request.method == 'POST':
+        url = request.form['url']
+        store_bookmark(url)
+        flash('stored bookmark: {}'.format(url))
+        return redirect(url_for('index'))
+
     return render_template('add.html')
 
 @app.errorhandler(404)
