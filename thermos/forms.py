73b17ebdd8bdf75565f46290fbd2b12fdf08529f
kosa <kosinski.michal@gmail.com>
2015-10-05 14:54:50 +0200

    additional validation of links

--- thermos/forms.py
+++ thermos/forms.py
@@ -6,3 +6,20 @@ from wtforms.validators import DataRequired, url
 class BookmarkForm(Form):
     url = URLField('url', validators=[DataRequired(), url()])
     description = StringField('description')
+
+    def validate(self):
+
+        # add protocol suffix if not present
+        if not self.url.data.startswith('http://') or \
+               self.url.data.startswith('https://'):
+            self.url.data = 'http://' + self.url.data
+
+        # perform normal valiations
+        if not Form.validate(self):
+            return False
+
+        # if description field is empty, fill it with url string
+        if not self.description.data:
+            self.description.data = self.url.data
+
+        return True
