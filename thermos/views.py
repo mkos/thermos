c409bb68f9c612d9f3b1a261db9f86620fbf1a02
kosa <kosinski.michal@gmail.com>
2016-03-22 13:15:58 +0100

    add possibility to edit bookmarks

--- thermos/views.py
+++ thermos/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash, request, abort
 from forms import BookmarkForm, LoginForm, SignupForm
 from thermos import app, db, login_manager
 from models import User, Bookmark
@@ -34,7 +34,22 @@ def add():
         flash('stored bookmark: {}'.format(description))
         return redirect(url_for('index'))
 
-    return render_template('add.html', form=form)
+    return render_template('bookmark_form.html', form=form, title="Add a bookmark")
+
+
+@app.route('/edit/<int:bookmark_id>', methods=['GET', 'POST'])
+@login_required
+def edit_bookmark(bookmark_id):
+    bookmark = Bookmark.query.get_or_404(bookmark_id)
+    if current_user != bookmark.user:
+        abort(403)
+    form = BookmarkForm(obj=bookmark)
+    if form.validate_on_submit():
+        form.populate_obj(bookmark)
+        db.session.commit()
+        flash("Stored '{}'".format(bookmark.description))
+        return redirect(url_for('user', username=current_user.username))
+    return render_template('bookmark_form.html', form=form, title="Edit bookmark")
 
 
 @app.route('/user/<username>')
