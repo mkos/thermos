1797238b29caed66f67aa01de8f7a748b9e49c73
kosa <kosinski.michal@gmail.com>
2016-03-25 11:40:53 +0100

    fix query performance for edit bookmark

--- thermos/models.py
+++ thermos/models.py
@@ -18,7 +18,7 @@ class Bookmark(db.Model):
                                                             # is added rather than always having the same value
     description = db.Column(db.String(300))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    _tags = db.relationship('Tag', secondary=tags,
+    _tags = db.relationship('Tag', secondary=tags, lazy='joined',
                             backref=db.backref('bookmarks', lazy='dynamic'))
 
     def __repr__(self):
